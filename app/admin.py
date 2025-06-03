208c549253dbbcd79acbdd9335a12913aec13d81
yasminnn19 <yasminrgoulart12@gmail.com>
2025-06-03 12:18:48 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from .models import *
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Livro, Autor
+
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1  # Número de livros adicionais para adicionar no admin
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)  # Campos que serão exibidos na listagem
+    search_fields = ('nome',)  # Campos que serão pesquisados
+    inlines = [LivroInline]  # Adiciona a tabela de livros no admin de autores
+
+admin.site.register(Livro)
+admin.site.register(Autor, AutorAdmin)
 admin.site.register(Cidade)
-admin.site.register(Autor)
 admin.site.register(Editora)
 admin.site.register(Leitor)
-admin.site.register(Livro)
 admin.site.register(Genero)
--- app/models.py
+++ app/models.py
@@ -47,7 +47,10 @@ class Leitor(models.Model):
         verbose_name_plural = "Leitores"
 
 class Genero(models.Model):
-    nome = models.CharField(max_length=100, verbose_name="Gênero")
+    nome = models.CharField(
+        max_length=100,
+        verbose_name="Gênero"
+    )
 
     def __str__(self):
         return self.nome
@@ -57,13 +60,34 @@ class Genero(models.Model):
         verbose_name_plural = "Gêneros"
 
 class Livro(models.Model):
-    nome = models.CharField(max_length=100, verbose_name="Nome do livro")
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, verbose_name="Autor do livro")
-    editora = models.ForeignKey(Editora, on_delete=models.CASCADE, verbose_name="Editora do livro")
-    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, verbose_name="Gênero do livro")
-    preco = models.IntegerField(verbose_name="Preço do livro")
-    data_plub = models.DateField(verbose_name="Data de publicação do livro")
-    status = models.BooleanField(verbose_name="Status do livro")
+    nome = models.CharField(
+        max_length=100,
+        verbose_name="Nome do livro"
+    )
+    autor = models.ForeignKey(
+        Autor,
+        on_delete=models.CASCADE,
+        verbose_name="Autor do livro"
+    )
+    editora = models.ForeignKey(
+        Editora,
+        on_delete=models.CASCADE,
+        verbose_name="Editora do livro"
+    )
+    genero = models.ForeignKey(
+        Genero,
+        on_delete=models.CASCADE,
+        verbose_name="Gênero do livro"
+    )
+    preco = models.IntegerField(
+        verbose_name="Preço do livro"
+    )
+    data_plub = models.DateField(
+        verbose_name="Data de publicação do livro"
+    )
+    status = models.BooleanField(
+        verbose_name="Status do livro"
+    )
 
     def __str__(self):
         return f'{self.nome}, {self.autor}'
